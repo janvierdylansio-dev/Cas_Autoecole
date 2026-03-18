b08956c7f1fac72177708754d5e93f940b8b1b6e
janvierdylansio-dev <janvier.dylan.sio@gmail.com>
2026-03-18 10:45:13 +0100

    Ajoutez des fichiers projet.

---
+++ Cas_Autoecole/Eleve.cs
@@ -0,0 +1,24 @@
+﻿namespace Cas_Autoecole
+{
+    public class Eleve
+    {
+        private string Nom { get; set; }
+        private string prenom;
+        private DateTime dateInsription;
+        private int credit;
+
+
+        public Eleve(string nom, string prenom, DateTime dateinscription, int credit)
+        {
+            this.Nom = nom;
+            this.prenom = prenom;
+            this.dateInsription = dateinscription;
+        }
+        public override string ToString()
+        {
+            return $"{Nom}";
+        }
+
+
+    }
+}
---
+++ cslAutoecole/Program.cs
@@ -0,0 +1,3 @@
+﻿using Cas_Autoecole;
+Eleve eleve1 = new Eleve("toto", "robert", new DateTime(2025, 1, 1), 25);
+Console.WriteLine(eleve1.ToString());
